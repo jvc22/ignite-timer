96a7ac7dbc1a9535be88319fe7eb14a439c2087f
joaovcostas <jvc22102003@gmail.com>
2024-01-11 11:46:24 -0300

    exported context and organized files

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,17 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { Router } from './Router'
 import { BrowserRouter } from 'react-router-dom'
+import { CyclesContextProvider } from './contexts/CyclesContext'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <BrowserRouter>
-        <Router />
-        <GlobalStyle />
+        <CyclesContextProvider>
+          <Router />
+        </CyclesContextProvider>
       </BrowserRouter>
+      <GlobalStyle />
     </ThemeProvider>
   )
 }
